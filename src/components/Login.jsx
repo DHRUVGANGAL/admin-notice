45f65fd7f9b90435057ecba562cb920d267fcf8d
DHRUVGANGAL <dhruvgangal123@gmail.com>
2025-04-10 13:21:16 +0530

    logo fix

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -173,7 +173,7 @@ const Login = () => {
       <div className="max-w-md w-full space-y-8 bg-white p-8 rounded-lg shadow-md">
         <div>
           <div className="flex justify-center">
-            <img src="../public/images.png" alt="Company Logo" className="h-22 w-auto" />
+            <img src="/images.png" alt="Company Logo" className="h-22 w-auto" />
           </div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Sign in to your account
